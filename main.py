19f7fa171f9382fb288040696302db395c4d7546
Sweety-143 <112478761+Sweety-143@users.noreply.github.com>
2022-09-05 11:00:07 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st 
+import pickle
+pickle_in=open('SalaryPrediction.pkl','rb')
+model=pickle.load(pickle_in)
+
+years=st.number_input('Years of Experience')
+if st.button('PREDICT'):
+  Salary=model.predict([[Years]])
+  st.success(f'SALARYPREDICTED IS {Salary}')
